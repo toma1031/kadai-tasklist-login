c6dfcabbc60f2013736fdcbd657d6106cdc58ca1
akinori.toma <ignitoma_1031@yahoo.co.jp>
2020-05-22 12:33:26 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,10 @@
 Rails.application.routes.draw do
-#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-#   get 'tasks/:id', to: 'tasks#show'
-#   post 'tasks', to: 'tasks#create'
-#   put 'tasks/:id', to: 'tasks#update'
-#   delete 'tasks/:id', to: 'tasks#destroy'
-  
-#     # index: show の補助ページ
-#   get 'tasks', to: 'tasks#index'
 
-#   # new: 新規作成用のフォームページ
-#   get 'tasks/new', to: 'tasks#new'
 
-#   # edit: 更新用のフォームページ
-#   get 'tasks/:id/edit', to: 'tasks#edit'
-
-root to: 'tasks#index'
-
-resources :tasks
+    root to: 'tasks#index'
+    
+    get 'signup', to: 'users#new'
+    resources :users, only: [:new, :create]
+    
+    resources :tasks
 end
